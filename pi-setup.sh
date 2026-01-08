266a09620c00861fb1f04d9464868bb34905a35d
Eyeball Edward <edwardyeung39@gmail.com>
2026-01-08 21:14:40 +1100

    add systemd unit and hotspot

--- pi-setup.sh
+++ pi-setup.sh
@@ -21,3 +21,19 @@ git clone https://github.com/eyeballcode/sitski-sensor.git
 cd sitski-sensor/
 npm i -d
 
+printf '''[Unit]
+Description=SitSki Server
+
+[Service]
+User=onboard
+ExecStart=/home/monash/sitski-sensor/start.sh
+Restart=always''' | sudo tee /etc/systemd/system/sitski.service
+
+sudo nmcli con add type wifi ifname wlan0 con-name 'Sit Ski' autoconnect yes ssid 'Sit Ski'
+sudo nmcli con modify 'Sit Ski' 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+sudo nmcli con modify 'Sit Ski' wifi-sec.key-mgmt wpa-psk
+sudo nmcli con modify 'Sit Ski' wifi-sec.proto rsn
+sudo nmcli con modify 'Sit Ski' 802-11-wireless-security.proto rsn
+sudo nmcli con modify 'Sit Ski' 802-11-wireless-security.pairwise ccmp
+sudo nmcli con modify 'Sit Ski' wifi-sec.psk "fit2082 sit ski"
+# sudo nmcli con up 'Sit Ski'
